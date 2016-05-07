31f3595bdc2ead51b8fd7c29bb86238e4a3937b3
viseshraj <viseshraj@yahoo.com>
2016-05-07 14:49:55 -0400

    updated About page

--- Server.R
+++ Server.R
@@ -14,6 +14,8 @@ shinyServer(
     output$About <- renderText("<b>DOCUMENTATION:</b> <br/><br/>The goal of this application is to carryout simple arithematic operation. <br/>The Steps are mentioned below, <ul><li>Enter two values</li>
                                <li>Select the desired operation</li>
                                <li>Check the checkbox if you need the absolute value</li>
-                               <li>Click on Calculate</li></ul>")
+                               <li>Click on Calculate</li></ul>
+                               <br/><br/>
+                               To access the script for this app from GitHub repo <a href='https://github.com/viseshraj/DataProductsAppAssignment.git'>click here</a> ")
   }
 )
